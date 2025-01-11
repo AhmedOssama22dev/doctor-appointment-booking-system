e492fc786e1c2017b335044357ed6e14940ba187
Ahmed Ossama <ahmedossama640@gmail.com>
2025-01-12 00:34:01 +0200

    create slots table

---
+++ database/migrations/2025_01_11_211310_create_slots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->string('guid')->unique()->default((string) Str::orderedUuid());
+            $table->dateTime('time');
+            $table->string('doctor_name')->nullable()->default("Dr. Main Doctor");
+            $table->boolean('is_reserved')->default(false);
+            $table->decimal('cost');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slots');
+    }
+};
