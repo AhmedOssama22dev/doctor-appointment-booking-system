27ff9c3e37d832f0e82626b512b0b8de59ccbf9e
Ahmed Ossama <ahmedossama640@gmail.com>
2025-01-17 02:05:49 +0200

    Add HasFactory trait to Slot model for improved factory support

--- app/Modules/Availability/Models/Slot.php
+++ app/Modules/Availability/Models/Slot.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Modules\Availability\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Slot extends Model
 {
+    use HasFactory;
     protected $table = 'slots';
     protected $fillable = ['guid', 'time', 'doctor_name', 'is_reserved', 'cost'];
     // let's assume that all slots are one hour long to avoid complexity and adding duration field
@@ -14,4 +16,5 @@ class Slot extends Model
     {
         return $this->hasOne('App\Modules\AppointmentBooking\Infrastructure\Models\Appointment');
     }
+
 }
